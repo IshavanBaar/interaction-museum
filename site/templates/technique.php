1c187d50499fd2c55e737ec6dc41a6986d6f8405
lipcanu <diana.lipcanu@gmail.com>
2016-04-13 17:21:54 +0200

    technique working

--- site/templates/technique.php
+++ site/templates/technique.php
@@ -1,40 +1,44 @@
 <?php snippet('header') ?>
 
- <div class="container" role="main">
-	<div class="technique-page">
+<div class="container" role="main">
+	<div class="row">
 		
 		<!-- HEADER IMAGE -->
 		<?php 
-			$header_image = (string)$page->header_image();
-			$image = $page->image($header_image);
-			$alternative = $page->images()->sortBy('sort', 'asc')->first(); 
+		$header_image = (string)$page->header_image();
+		$image = $page->image($header_image);
+		$alternative = $page->images()->sortBy('sort', 'asc')->first(); 
 			//TODO fix if no image is there
 		?>
 
 		<figure>
 			<img src="<?php echo file_exists($image) ? $image->url() : $alternative->url(); ?>" 
-			alt="">
+			alt="" class="col-xs-12">
 		</figure>
 		
-		<!-- TITLE -->
-		<h1><?php echo $page->title()->html() ?></h1>
-		
-		<!-- DESCRIPTION -->
-		<div class="text">
-		  <?php echo $page->description()->kirbytext() ?>
-		</div>
-		
-		<!-- TRADE-OFFS/COMPARISON -->
-		<h3>Trade offs  Comparison</h3>
-		
-		<!-- TAGS -->
-		<ul class="meta cf">
-		  <!--<li><b>Year:</b> <time datetime="<?php echo $page->date('c') ?>"><?php echo $page->date('Y', 'year') ?></time></li>-->
-		  <li><b>Tags:</b> <?php echo $page->tags() ?></li>
-		</ul>
+		<div class="col-lg-8 col-lg-offset-2">
+			<!-- TITLE -->
+			<h1><?php echo $page->title()->html() ?></h1>
 
-		<!-- NEXT / PREV -->
-		<nav class="nextprev cf" role="navigation">
+			<!-- DESCRIPTION -->
+			<div class="text">
+				<?php echo $page->description()->kirbytext() ?>
+			</div>
+
+			<!-- TRADE-OFFS/COMPARISON -->
+			<h3>Trade offs  Comparison</h3>
+
+			<!-- TAGS -->
+			<ul class="meta ">
+				<!--<li><b>Year:</b> <time datetime="<?php echo $page->date('c') ?>"><?php echo $page->date('Y', 'year') ?></time></li>-->
+				<li><b>Tags:</b> <?php echo $page->tags() ?></li>
+			</ul>
+		</div>
+
+<!-- NEXT / PREV 
+		<div class="col-lg-2, col-lg-offset-5">
+		
+		<nav class="nextprev " role="navigation">
 		  <?php if($prev = $page->prevVisible()): ?>
 		  <a class="prev" href="<?php echo $prev->url() ?>">&larr; previous</a>
 		  <?php endif ?>
@@ -42,7 +46,8 @@
 		  <a class="next" href="<?php echo $next->url() ?>">next &rarr;</a>
 		  <?php endif ?>
 		</nav>
-	</div>
-  </div>
+	</div>-->
+</div>
+</div>
 
 <?php snippet('footer') ?>
