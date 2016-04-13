1c86798809e24b2ef0bea113f67bf61cd0a1f2af
lipcanu <diana.lipcanu@gmail.com>
2016-04-13 15:06:00 +0200

    fixed the issue

--- site/snippets/recently-added.php
+++ site/snippets/recently-added.php
@@ -1,24 +1,23 @@
 <div class="row">
-<h2 class="col-xs-12"><?php echo page('recently-added')->title()->html() ?></h2>
+      <h2  class="col-xs-12"><?php echo page('recently-added')->title()->html() ?></h2>
+
+
   <?php foreach(page('recently-added')->children()->visible()->limit(100) as $technique): ?>
-    <div class="col-sm-4">
-    <h2><?php echo $technique->title()->html() ?></h2>
-      <p><?php echo $technique->text()->excerpt(80) ?></p>
-      <p><a class="btn btn-default" href="<?php echo $technique->url() ?>" role="button">View details &raquo;</a></p>
-     
-      <?php if($image = $technique->images()->sortBy('sort', 'asc')->first()): ?>
-     
-        <a href="<?php echo $technique->url() ?>" class="thumbnail">
-          <img src="<?php echo $image->url() ?>" alt="<?php echo $technique->title()->html() ?>" >
-        </a>
-      <?php endif ?>
-    </div>
-  <?php endforeach ?>
+  <div class="col-sm-4">
+  <!-- HEADER IMAGE -->
+  <?php 
+    $header_image = (string)$technique->header_image();
+    $image = $technique->image($header_image);
+    $alternative = $technique->images()->sortBy('sort', 'asc')->first(); 
+    //TODO fix if no image is there
+  ?>
+  
+  <!-- THUMBNAIL LINK -->
+    <a href="<?php echo $technique->url() ?>">
+      <img src="<?php echo file_exists($image) ? $image->url() : $alternative->url(); ?>" alt="">
+    <h5><a href="<?php echo $technique->url() ?>"><?php echo $technique->title()->html() ?></a></h5>
+    </a>
 </div>
+  <?php endforeach ?>
 
-
-
-
-
-
-      
+</div>
