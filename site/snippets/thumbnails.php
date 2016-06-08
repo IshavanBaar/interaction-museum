df7484a9d39413ffed049c588fd64c65a349eb7b
Isha van Baar <isha.vanbaar@gmail.com>
2016-06-08 14:41:31 +0200

    + buttons

--- site/snippets/thumbnails.php
+++ site/snippets/thumbnails.php
@@ -9,10 +9,16 @@
 	<!-- THUMBNAIL LINK -->
 	<div class="col-md-4 col-sm-6">
 		<div class="thumbnail">
-			<a href="<?php echo $technique->url() ?>">
-				<!-- TODO fix if there is no gif file in the folder -->
+            <!-- Button to add to collection -->
+            <button style="position:absolute; right:20px;" id="sidebar_toggle" class="btn btn-default btn-primary" type="submit">
+                <span class="glyphicon glyphicon-plus"></span>
+            </button>
+            
+            <a href="<?php echo $technique->url() ?>">
+                <!-- TODO fix if there is no gif file in the folder -->
 				<img src="<?php echo $image->url();?>" alt=""
 				onmouseover="play(this);" onmouseout="stop(this);">
+
 				<p class="caption"><?php echo $technique->title()->html() ?></p>
 			</a>
 		</div>
