e246ac23e844a2cf88220f87adceee1ac745fa16
Isha van Baar <isha.vanbaar@gmail.com>
2016-06-21 13:59:15 +0200

    New collection as a submenu

--- site/snippets/menu-items.php
+++ site/snippets/menu-items.php
@@ -4,18 +4,17 @@
       <a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
     </li>
 <?php endforeach ?>
-
-<!-- CREATE A NEW COLLECTION -->  
-<?php if($user = $site->user()): ?>  
-    <li>
-      <a class="new_collection" href="">New collection</a>
-    </li>
-<?php endif; ?>  
   
 <!-- ACCOUNT NAME / LOGOUT -->
 <?php if($user = $site->user()): ?>
     <li>
-      <a href="<?php echo url('account') ?>"><?php echo $user?></a>
+        <a href="<?php echo url('account') ?>"><?php echo $user?></a>
+        <ul class="submenu">
+            <!-- CREATE A NEW COLLECTION -->  
+            <li>
+                <a class="new_collection" href="">New collection</a>
+            </li>
+        </ul>
     </li>
 <?php else: ?>
     <li>
