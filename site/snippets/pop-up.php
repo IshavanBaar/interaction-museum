21b27812d76b698ca87d0430100b23a35b935066
lipcanu <diana.lipcanu@gmail.com>
2016-07-29 11:22:11 +0200

    popup search works

---
+++ site/snippets/pop-up.php
@@ -0,0 +1,8 @@
+<div id="pop-up">
+	
+
+<?php snippet('search-bar-exhibit') ?>
+                    <!-- Search results -->
+<?php snippet('search') ?>
+
+</div>
--- site/templates/exhibit-editor.php
+++ site/templates/exhibit-editor.php
@@ -3,22 +3,20 @@
                 <!-- Menu -->
             <?php snippet('menu')?>
 
-            <?php snippet('search-bar-exhibit') ?>
-                    <!-- Search results -->
-            <?php snippet('search') ?>
     </div>
 
 
     <div class="main container" role="main">
-
+        <?php snippet('pop-up') ?>
         <div id="editor" class="container">
             <input type="text" id="editor-title" class="col-md-6 col-md-offset-2" placeholder="Title">
+            
+            <div id="editor-content" class="editable col-xs-12"></div>
             <a id="publish_exhibit_btn" class="btn btn-primary col-md-2">Publish</a>
-            <div id="editor-content" class="editable">
-                
-            </div>
+            <a id="add_interaction_btn" class="btn btn-primary col-md-2">Add Interaction</a>
         </div>
-
+         
+         
         <script>
             var editor = new MediumEditor('.editable');
 
