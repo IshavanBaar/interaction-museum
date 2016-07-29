ff3f936190612ed2a5a2f76922e0ab3d775891fe
Isha van Baar <isha.vanbaar@gmail.com>
2016-07-29 12:56:27 +0200

    deleted rangy

--- site/templates/exhibit-editor.php
+++ site/templates/exhibit-editor.php
@@ -1,64 +1,77 @@
 <?php snippet('header-exhibit-creator') ?>
-    <div class="header">
-                <!-- Menu -->
-            <?php snippet('menu')?>
+    
+<div class="header">
+    <!-- Menu -->
+    <?php snippet('menu')?>
+</div>
 
+<div class="main container" role="main">
+    <?php snippet('pop-up') ?>
+    <div id="editor" class="container">
+        <input type="text" id="editor-title" class="col-md-6 col-md-offset-2" placeholder="Title">
+
+        <div id="editor-content" class="editable col-xs-12"></div>
+        <a id="publish_exhibit_btn" class="btn btn-primary col-md-2">Publish</a>
+        <a id="add_interaction_btn" class="btn btn-primary col-md-2">Add Interaction</a>
     </div>
 
 
-    <div class="main container" role="main">
-        <?php snippet('pop-up') ?>
-        <div id="editor" class="container">
-            <input type="text" id="editor-title" class="col-md-6 col-md-offset-2" placeholder="Title">
-            
-            <div id="editor-content" class="editable col-xs-12"></div>
-            <a id="publish_exhibit_btn" class="btn btn-primary col-md-2">Publish</a>
-            <a id="add_interaction_btn" class="btn btn-primary col-md-2">Add Interaction</a>
-        </div>
-         
-         
-        <script>
-            //rangy.init();
-            
-            var AddTechniqueButton = MediumEditor.Extension.extend({
-                name: 'addtechnique',
-                
-                init: function () {
-                    this.button = this.document.createElement('button');
-                    this.button.classList.add('medium-editor-action');
-                    this.button.innerHTML = '<i class="fa fa-search"></i> Add Technique';
-                    this.button.title = 'Add technique';
-                    this.button.id = 'search-technique-btn';
-                    this.button.addEventListener('click', function() {
-                        //var selection = this.classApplier.toggleSelection();
-                        console.log("button clicked");
-                        console.log(rangy.saveSelection());
-                        console.log(this.classApplier.getSelection());
-                    });
-                },
+    <script>
+        rangy.init();
 
-                getButton: function () {
-                    return this.button;
-                }
+        var AddTechniqueButton = MediumEditor.Extension.extend({
+            name: 'addtechnique',
 
-            });
-            
-            var editor = new MediumEditor('.editable', {
-              toolbar: {
-                buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3', 'quote', 'addtechnique']
-              },
-              extensions: {
-                'addtechnique': new AddTechniqueButton()
-              }
-            });
-
-            $(function () {
-                $('.editable').mediumInsert({
-                    editor: editor
+            init: function () {
+                this.button = this.document.createElement('button');
+                this.button.classList.add('medium-editor-action');
+                this.button.innerHTML = '<i class="fa fa-search"></i> Add Technique';
+                this.button.title = 'Add technique';
+                this.button.id = 'search-technique-btn';
+                this.button.addEventListener('click', function() {
+                    //var selection = this.classApplier.toggleSelection();
+                    console.log("button clicked");
+                    console.log(getSelectionText());
+                    showPopUp(getSelectionText());
                 });
+            },
+
+            getButton: function () {
+                return this.button;
+            }
+
+        });
+
+        var editor = new MediumEditor('.editable', {
+            toolbar: {
+                buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3', 'quote', 'addtechnique']
+            },
+            extensions: {
+                'addtechnique': new AddTechniqueButton()
+            }
+        });
+        
+        function getSelectionText() {
+            var text = "";
+            if (window.getSelection) {
+                text = window.getSelection().toString();
+            } else if (document.selection && document.selection.type != "Control") {
+                text = document.selection.createRange().text;
+            }
+            return text;
+        }
+        
+        function showPopUp(text) {
+            $('#pop-up').show();
+        }
+        
+        $(function () {
+            $('.editable').mediumInsert({
+                editor: editor
             });
-        </script>
-    </div>
+        });
+    </script>
+</div>
 
 
 <?php snippet('footer') ?> 
