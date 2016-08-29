cf2a07bdd2f6ad9122ebbcebd50bc1ddd4b7125f
lipcanu <diana.lipcanu@gmail.com>
2016-08-29 15:44:37 +0200

    semantic search - not working

--- site/snippets/search-bar.php
+++ site/snippets/search-bar.php
@@ -13,4 +13,37 @@
         </div>
       </form>
   </div>
+  
+</div>
+
+<div class="row">
+  <div id="semantic-search" class="col-lg-6 col-lg-offset-3">
+    <span class="sentence-element">Show me </span>
+    <div class="sentence-select">
+      <div class="input-group-btn">
+        <button class="btn dropdown-toggle " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">techniques <span class="caret"></span></button>
+         <ul class="options dropdown-menu">
+          <li class="option"><a href="#">techniques</a></li>
+          <li class="option"><a href="#">ecollections</a></li>
+          <li class="option"><a href="#">exhibits</a></li>
+          <li class="option"><a href="#">all</a></li>
+        </ul>
+      </div><!-- /btn-group -->
+    </div><!-- /input-group -->
+
+
+
+   
+    <span  class="sentence-element"> that </span>
+     <div class="sentence-select">
+      <div class="input-group-btn">
+        <button class="btn dropdown-toggle " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">use the technology of <span class="caret"></span></button>
+         <ul class="options dropdown-menu">
+          <li class="option"><a href="#">use the technology of</a></li>
+          <li class="option"><a href="#">help users to</a></li>
+          <li class="option"><a href="#">solve the design problem of</a></li>
+        </ul>
+      </div><!-- /btn-group -->
+  </div>
+</div>  <!-- end row -->
 </div>
